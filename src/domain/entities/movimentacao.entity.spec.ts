8cdd2cfc6ddf666ca9e1fe9b91075b79ce92dcc2
jpmoncao <pedrohjoao44@gmail.com>
2025-12-17 08:22:49 -0300

    feat: movimentacao tests

---
+++ src/domain/entities/movimentacao.entity.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import { Movimentacao } from "./movimentacao.entity";
+import { ValidationError } from "../../core/errors/validation.error";
+
+describe('Movimentacao Entity', () => {
+    it('deve criar um movimentação com a data e hora atual', () => {
+        const movimentacao = new Movimentacao('Teste movimentação');
+
+        expect(movimentacao.descricao).toEqual('Teste movimentação');
+        expect(movimentacao.data).toBeInstanceOf(Date);
+    });
+
+    it('deve disparar um ValidationError ao criar descrição com menos de 5 caracteres', () => {
+        expect(() => {
+            new Movimentacao('Test');
+        }).toThrow(ValidationError);
+        expect(() => {
+            new Movimentacao('Test');
+        }).toThrow('A descrição da movimentação deve ter no mínimo 5 caracteres.');
+    })
+});
