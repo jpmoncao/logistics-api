2a7e148d4498675dad28b839498920e827ea304a
jpmoncao <pedrohjoao44@gmail.com>
2025-12-18 09:29:10 -0300

    fix: alter handler

--- src/infra/factories/concluir-entrega.factory.ts
+++ src/infra/factories/concluir-entrega.factory.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../database/prisma/client";
 import { PrismaEntregaRepository } from "../database/prisma/repositories/prisma-entrega.repository"
 import { DomainEventDispatcher } from "../../core/events/dispatcher";
-import { enviarEmailDespachoHandler } from "../../application/handlers/enviar-email-despacho.handler";
+import { enviarEmailEntregaConcluidaHandler } from "../../application/handlers/enviar-email-entrega-concluida.handler";
 import { ConcluirEntregaUseCase } from "../../application/use-cases/concluir-entrega.usecase";
 import { ConcluirEntregaController } from "../http/controllers/concluir-entrega.controller";
 import { EntregaConcluidaEvent } from "../../domain/events/entrega-concluida.event";
@@ -10,7 +10,7 @@ export function concluirEntregaFactory(): ConcluirEntregaController {
     const repository = new PrismaEntregaRepository(prisma);
 
     const dispatcher = new DomainEventDispatcher();
-    dispatcher.register(EntregaConcluidaEvent.eventName, enviarEmailDespachoHandler)
+    dispatcher.register(EntregaConcluidaEvent.eventName, enviarEmailEntregaConcluidaHandler)
 
     const useCase = new ConcluirEntregaUseCase(repository, dispatcher);
     const controller = new ConcluirEntregaController(useCase);
